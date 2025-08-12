cedce86d529ed6ed27b2044b1192c1f1a4318e02
Navil Talukdar <naviltalukdar3@gmail.com>
2025-08-12 15:37:37 -0400

    hamburger menu complete

--- Personal_Website/src/Components/Navbar.jsx
+++ Personal_Website/src/Components/Navbar.jsx
@@ -8,14 +8,14 @@ function Navbar() {
   return (
     <div className='lg:mx-7 sm:mx-7 mx-10 mt-6 flex relative justify-between font-mono'>
       <Link to='/'><h1 className='font-mono font-bold mt-2 text-2xl hover:text-pink-100 cursor-pointer'>Navil Talukdar</h1></Link>
-      <div className=''>
+      <div >
         <Hamburger toggled={open} toggle={setOpen}/>
         {open && 
         <div className='text-pink-100 fixed top-0 left-0 z-50 bg-blue-950 h-screen w-screen px-5 py-5 space-y-18'>
-          <div className='lg:pl-271 pt-1 sm:pl-159 pl-98'>
+          <div className='absolute top-5 right-8 pt-1'>
             <Hamburger size={35} toggled={open} toggle={setOpen} />
           </div>
-          <ul className='text-center space-y-15 text-5xl'>
+          <ul className='pt-35 text-center space-y-15 text-5xl'>
             <li className='hover:text-blue-300 cursor-pointer'>Projects</li>
             <li className='hover:text-blue-300 cursor-pointer'>Resume</li>
             <li className='hover:text-blue-300 cursor-pointer'>Contact</li>
