aec6c7908bbe242c7865144925704735e7bb8310
Navil Talukdar <naviltalukdar3@gmail.com>
2025-08-12 01:27:40 -0400

    starting hamburger menu

--- Personal_Website/src/Components/Navbar.jsx
+++ Personal_Website/src/Components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import HamburgerMenu from './HamburgerMenu';
 
 function Navbar() {
   const [open, setopen] = useState(false);
   
   return (
     <div className='mx-6 mt-7 flex justify-between'>
-      <h1 className='font-bold text-xl'>NAVIL TALUKDAR</h1>
+      <Link to='/'><h1 className='font-bold text-xl hover:text-blue-300 cursor-pointer'>NAVIL TALUKDAR</h1></Link>
       <div className='text-3xl'>
-        -
         {/* HAMBURGER MENU */}
       </div>
     </div>
