1c23033bc551b9e2d489e1145675a637455e3157
Navil Talukdar <naviltalukdar3@gmail.com>
2025-08-13 02:10:46 -0400

    herosection update

--- Personal_Website/src/Components/Herosection.jsx
+++ Personal_Website/src/Components/Herosection.jsx
@@ -4,7 +4,10 @@ import profile from '../assets/ProfilePic.JPG'
 function HeroSection() {
   return (
     <div className='lg:mx-7 sm:mx-7 mx-10 mt-6 flex relative justify-center font-mono' >
-        <img src={profile} />
+        <div>
+            <h1>THIS IS ME</h1>
+        </div>
+        <img className='w-52 h-62 rounded-4xl border border-pink-200' src={profile} alt='Navil Talukdar'/>
     </div>
   )
 }
