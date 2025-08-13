ff62a25e5a0d83bb92fd7b8e411556ebaad0a52f
Navil Talukdar <naviltalukdar3@gmail.com>
2025-08-13 01:56:30 -0400

    media icons complete

--- Personal_Website/src/Components/Navbar.jsx
+++ Personal_Website/src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { useState } from 'react'
 import Hamburger from 'hamburger-react'
+import { SocialIcon } from 'react-social-icons'
 
 function Navbar() {
   const[ open, setOpen ] = useState(false)
@@ -12,15 +13,20 @@ function Navbar() {
         <Hamburger toggled={open} toggle={setOpen}/>
         {open && 
         <div className='text-pink-100 fixed top-0 left-0 z-50 bg-blue-950 h-screen w-screen px-5 py-5 space-y-18'>
-          <div className='absolute top-5 right-8 pt-1'>
+          <div className='absolute top-5 lg:right-8 pt-1 right-10'>
             <Hamburger size={35} toggled={open} toggle={setOpen} />
           </div>
-          <ul className='pt-35 text-center space-y-15 text-5xl'>
+          <ul className='pt-30 text-center space-y-15 text-4xl lg:text-5xl'>
             <li className='hover:text-blue-300 cursor-pointer'>Projects</li>
             <li className='hover:text-blue-300 cursor-pointer'>Resume</li>
             <li className='hover:text-blue-300 cursor-pointer'>Contact</li>
           </ul>
-          {/* Icons */}
+          <div className=' pt-24 flex justify-center space-x-7 text-5xl'>
+            <SocialIcon url='www.instagram.com' href='https://www.instagram.com/navil2468/' target='_blank '/>
+            <SocialIcon url='www.linkedin.com' href='www.linkedin.com/in/navil-talukdar' target='_blank '/>
+            <SocialIcon url='www.github.com' href='https://github.com/navil2468' target='_blank'/>
+            <SocialIcon url='www.email.com' href='mailto:naviltalukdar3@gmail.com' target='_blank'/>
+          </div>
         </div>
         }
       </div>
