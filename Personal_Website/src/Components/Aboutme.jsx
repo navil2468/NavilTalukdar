f6ebd9c3ed90fbedf99081e7f8b36540795eec25
Navil Talukdar <naviltalukdar3@gmail.com>
2025-08-15 01:25:17 -0400

    need to fix small/mobile screens for about me page and update style for herosection

--- Personal_Website/src/Components/Aboutme.jsx
+++ Personal_Website/src/Components/Aboutme.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Aboutme() {
   return (
     <div className='lg:mx-7 sm:mx-7 mx-10 mt-45 lg:flex relative lg:justify-center font-mono' >
-        <div className='text-center flex items-center text-pink-200 w-150'>
+        <div className='text-center flex items-center text-pink-200 lg:w-150 max-w-screen'>
            <ul className='lg:space-y-3 text-start list-disc text-4xl'>
             <li>Columbia University sophomore, majoring in Computer Science</li>
             <li>Lifelong puzzle-solver</li>
@@ -12,7 +12,7 @@ function Aboutme() {
             <li>Loves challeneges and riddles</li>
            </ul>
         </div>
-        <div className='bg-pink-100 p-5 px-10 text-neutral-900 rounded-4xl'>
+        <div className='bg-pink-100 p-5 px-10 text-neutral-900 text-center rounded-4xl'>
             <h1 className='text-9xl'>WHO</h1>
             <h1 className='text-9xl'>AM</h1>
             <h1 className='text-9xl'>I❓</h1>
