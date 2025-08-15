49e80204912bc6c206d6b6901ce656c06befa267
Navil Talukdar <naviltalukdar3@gmail.com>
2025-08-15 01:20:49 -0400

    About me section complete

--- Personal_Website/src/Components/Aboutme.jsx
+++ Personal_Website/src/Components/Aboutme.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 function Aboutme() {
   return (
-    <div className='lg:mx-7 sm:mx-7 mx-10 mt-18 lg:flex relative lg:justify-center font-mono' >
-        <div className='mt-15 lg:space-y-3 text-center text-pink-200'>
-           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+    <div className='lg:mx-7 sm:mx-7 mx-10 mt-45 lg:flex relative lg:justify-center font-mono' >
+        <div className='text-center flex items-center text-pink-200 w-150'>
+           <ul className='lg:space-y-3 text-start list-disc text-4xl'>
+            <li>Columbia University sophomore, majoring in Computer Science</li>
+            <li>Lifelong puzzle-solver</li>
+            <li>Front-end developer</li>
+            {/* <li>Full-stack builder fluent in React, Node.js, Tailwind, and PostgreSQL</li> */}
+            <li>Loves challeneges and riddles</li>
            </ul>
         </div>
-        <div className='lg:mt-0 mt-15 lg:ml-35'>
+        <div className='bg-pink-100 p-5 px-10 text-neutral-900 rounded-4xl'>
             <h1 className='text-9xl'>WHO</h1>
             <h1 className='text-9xl'>AM</h1>
-            <h1 className='text-9xl max-lg:hidden'>I❔</h1>
+            <h1 className='text-9xl'>I❓</h1>
         </div>
     </div>
   )
