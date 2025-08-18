6534f4600cf71e4f369738c29445cdf986049c14
Navil Talukdar <naviltalukdar3@gmail.com>
2025-08-17 23:59:58 -0400

    tweaks

--- Personal_Website/src/Components/Aboutme.jsx
+++ Personal_Website/src/Components/Aboutme.jsx
@@ -5,7 +5,8 @@ function Aboutme() {
     <div className='lg:mx-7 sm:mx-7 mx-10 mt-45 lg:flex relative lg:justify-center font-mono' >
         <div className='text-center flex items-center text-pink-200 lg:w-150 max-w-screen'>
            <ul className='lg:space-y-3 text-start list-disc text-4xl'>
-            <li>Columbia University sophomore, majoring in Computer Science</li>
+            <li>Columbia University sophomore</li>
+            <li>CS Major</li>
             <li>Lifelong puzzle-solver</li>
             <li>Front-end developer</li>
             {/* <li>Full-stack builder fluent in React, Node.js, Tailwind, and PostgreSQL</li> */}
