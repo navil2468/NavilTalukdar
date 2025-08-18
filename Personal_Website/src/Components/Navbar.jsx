d74e17e8bd6e8d48034cff63304b429d3693454c
Navil Talukdar <naviltalukdar3@gmail.com>
2025-08-17 23:54:15 -0400

    fixing errors

--- Personal_Website/src/Components/Navbar.jsx
+++ Personal_Website/src/Components/Navbar.jsx
@@ -7,12 +7,12 @@ import { SocialIcon } from 'react-social-icons'
 function Navbar() {
   const[ open, setOpen ] = useState(false)
   return (
-    <div className='lg:mx-7 sm:mx-7 mx-10 mt-6 flex justify-between font-mono sticky top-0 z-50 py-3 backdrop-blur-lg '>
+    <div className='lg:mx-7 sm:mx-7 mx-10 mt-6 flex justify-between font-mono sticky top-0 z-150 py-3 backdrop-blur-lg '>
       <Link to='/'><h1 className='font-mono font-bold mt-2 text-2xl hover:text-pink-100 cursor-pointer'>Navil Talukdar</h1></Link>
       <div >
         <Hamburger toggled={open} toggle={setOpen}/>
         {open && 
-        <div className='text-neutral-800 fixed top-0 left-0 z-50 bg-pink-200 h-screen w-screen px-5 py-5 space-y-18'>
+        <div className='text-neutral-800 fixed top-0 left-0 z-50 bg-pink-200 h-screen w-screen space-y-18'>
           <div className='absolute top-5 lg:right-8 pt-1 right-10'>
             <Hamburger size={35} toggled={open} toggle={setOpen} />
           </div>  
