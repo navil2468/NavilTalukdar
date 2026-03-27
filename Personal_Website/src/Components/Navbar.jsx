d77da1da8061e00546377d0981b896a756183860
Navil Talukdar <naviltalukdar3@gmail.com>
2026-03-27 02:35:13 -0400

    starting new style change, herosection and navbar complete

--- Personal_Website/src/Components/Navbar.jsx
+++ Personal_Website/src/Components/Navbar.jsx
@@ -1,35 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { useState } from 'react'
-import Hamburger from 'hamburger-react'
 import { SocialIcon } from 'react-social-icons'
 
 
 function Navbar() {
-  const[ open, setOpen ] = useState(false)
   return (
-    <div className='px-7 flex justify-between sticky top-0 z-150 py-3 backdrop-blur-lg'>
-      <Link to={'/'}><h1 className='font-bold mt-2 text-2xl hover:text-pink-200 cursor-pointer'>Navil Talukdar</h1></Link>
-      <div>
-        <Hamburger toggled={open} toggle={setOpen}/>
-        {open && 
-        <div data-aos='fade-up' className='text-neutral-800 fixed top-0 left-0 z-50 bg-pink-100 h-screen w-screen space-y-18'>
-          <div className='absolute top-3 right-7'>
-            <Hamburger size={35} toggled={open} toggle={setOpen} />
-          </div>  
-          <ul className='pt-35 text-center space-y-15 text-4xl lg:text-5xl'>
-            <li className='hover:text-blue-300 cursor-pointer'><Link to={'../Projects'}>PROJECTS</Link></li>
-            <li className='hover:text-blue-300 cursor-pointer'><a href='https://drive.google.com/file/d/1pmkOkNW1e1dOgnGI6UOvH-2eps4aGBG7/view?usp=sharing' target='_blank'>RESUME</a></li>
-            <Link to={'../Contact'}><li className='hover:text-blue-300 cursor-pointer'>CONTACT</li></Link>
-          </ul>
-          <div className=' pt-24 flex justify-center space-x-7 text-5xl'>
-            <SocialIcon url='www.instagram.com' href='https://www.instagram.com/navil2468/' target='_blank '/>
-            <SocialIcon url='www.linkedin.com' href='www.linkedin.com/in/navil-talukdar' target='_blank '/>
-            <SocialIcon url='www.github.com' href='https://github.com/navil2468' target='_blank'/>
-            <SocialIcon url='www.email.com' href='mailto:naviltalukdar3@gmail.com' target='_blank'/>
-          </div>
-        </div>
-        }
+    <div className='p-7 flex justify-between sticky top-0 z-150 px-20 backdrop-blur-lg border-b border-gray-600'>
+    <Link to={'/'}><h1 className='font-bold text-2xl hover:text-pink-200 cursor-pointer'>Navil Talukdar</h1></Link>
+      <div className='flex space-x-5 pt-2'>
+        <p>About</p>
+        <p>Experience</p>
+        <p>Projects</p>
+        <p>Skills</p>
+        <p>Contact</p>
       </div>
     </div>
   )
